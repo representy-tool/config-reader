b7ee63fac000e53387fff514c29aeb40dfc6a22e
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2017-06-12 13:36:44 +0200

    handle temple error

--- test/read.test.js
+++ test/read.test.js
@@ -11,4 +11,16 @@ describe('Read file', () => {
     expect(config.sources.github.options).toBeInstanceOf(Array);
     expect(config.sources.github.options[0]).toBe(TOKEN_VALUE);
   });
+
+  test('test with empty context', () => {
+    // eslint-disable-next-line
+    const TOKEN = '${TOKEN}';
+    const config = ConfigReader.read('test/test.config.yml');
+    expect(config.folder).toBe('build');
+    expect(config.file).toBe('me.json');
+    expect(config.tokens.github).toBe(TOKEN);
+    expect(config.sources.github.user).toBe('salimkayabasi');
+    expect(config.sources.github.options).toBeInstanceOf(Array);
+    expect(config.sources.github.options[0]).toBe(TOKEN);
+  });
 });
